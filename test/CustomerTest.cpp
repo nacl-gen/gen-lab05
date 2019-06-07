dbe9cf8c7a1fbd5d84323e2b4b8a68fddb8690ed
Christoph Rueff <christoph.rueff@gmail.com>
2019-06-07 17:10:01 +0200

    Add CustomerTest with some integration tests

---
+++ test/CustomerTest.cpp
@@ -0,0 +1,56 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "../src/Customer.h"
+
+// Integration test suite for the Customers statement
+
+/**
+ * Integration
+ * Test statement of new Customer with name
+ */
+TEST(customer_integration_tests, statementNewCustomerWithName) {
+
+    Customer customer("CustomerName");
+
+    std::string result = customer.statement();
+    std::string expected = "Rental Record for CustomerName\nAmount owed is 0\nYou earned 0 frequent renter points";
+
+    EXPECT_EQ(result, expected);
+}
+
+/**
+ * Integration
+ * Test statement of new Customer without name
+ */
+TEST(customer_integration_tests, statementNewCustomerNoName) {
+
+    Customer customer;
+
+    std::string result = customer.statement();
+    std::string expected = "Rental Record for \nAmount owed is 0\nYou earned 0 frequent renter points";
+
+    EXPECT_EQ(result, expected);
+}
+
+/**
+ * Integration
+ * Test statement of Customer after some rentals
+ */
+TEST(customer_integration_tests, statementWithScenario) {
+
+    Customer customer("Olivier");
+
+    customer.addRental( Rental( Movie("Karate Kid"), 7));
+    customer.addRental( Rental( Movie( "Avengers: Endgame", Movie::NEW_RELEASE ), 5));
+    customer.addRental( Rental( Movie("Snow White", Movie::CHILDRENS), 3 ));
+
+    std::string result = customer.statement();
+    std::string expected = "Rental Record for Olivier\n\tKarate Kid\t9.5\n"
+                           "\tAvengers: Endgame\t15\n"
+                           "\tSnow White\t1.5\n"
+                           "Amount owed is 26\n"
+                           "You earned 4 frequent renter points";
+
+    EXPECT_EQ(result, expected);
+}
