f4687a8370d6b2b5520fd4c6f48e5c35da743e05
Christoph Rueff <christoph.rueff@gmail.com>
2019-06-14 21:02:51 +0200

    Change for() on iterators to a foreach() loop

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -12,30 +12,31 @@ string Customer::statement()
 {
     double totalAmount = 0;
     int frequentRenterPoints = 0;
-    vector< Rental >::iterator iter = _rentals.begin();
-    vector< Rental >::iterator iter_end = _rentals.end();
+
     ostringstream result;
     result << "Rental Record for " << getName() << "\n";
-    for ( ; iter != iter_end; ++iter ) {
+
+    for (const Rental &rental : _rentals) {
         double thisAmount = 0;
-        Rental each = *iter;
 
         // determine amounts for each line
-        thisAmount += each.computeAmount();
+        thisAmount += rental.computeAmount();
 
         // add frequent renter points
-        frequentRenterPoints++;
+        ++frequentRenterPoints;
 
         // add bonus for a two day new release rental
-        frequentRenterPoints += each.getBonusPoints();
+        frequentRenterPoints += rental.getBonusPoints();
 
         // show figures for this rental
-        result << "\t" << each << "\n";
+        result << "\t" << rental << "\n";
         totalAmount += thisAmount;
     }
+
     // add footer lines
     result << "Amount owed is " << totalAmount << "\n";
     result << "You earned " << frequentRenterPoints
            << " frequent renter points";
+
     return result.str();
 }
